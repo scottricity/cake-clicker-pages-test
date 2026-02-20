648bb9b45977c5dbc27ced303203d3b803b118e6
Timothy3773 <jonathanscott122@gmail.com>
2026-02-20 16:40:54 -0500

    yyes

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,20 +4,20 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771623079|199853983';
+const CACHE_VERSION = '1771623618|738474271';
 /** @type {string} */
 const CACHE_PREFIX = 'test2-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 /** @type {string} */
-const OFFLINE_URL = 'test1.offline.html';
+const OFFLINE_URL = 'index.offline.html';
 /** @type {boolean} */
 const ENSURE_CROSSORIGIN_ISOLATION_HEADERS = true;
 // Files that will be cached on load.
 /** @type {string[]} */
-const CACHED_FILES = ["test1.html","test1.js","test1.offline.html","test1.icon.png","test1.apple-touch-icon.png","test1.audio.worklet.js","test1.audio.position.worklet.js"];
+const CACHED_FILES = ["index.html","index.js","index.offline.html","index.icon.png","index.apple-touch-icon.png","index.audio.worklet.js","index.audio.position.worklet.js"];
 // Files that we might not want the user to preload, and will only be cached on first load.
 /** @type {string[]} */
-const CACHEABLE_FILES = ["test1.wasm","test1.pck","test1.side.wasm"];
+const CACHEABLE_FILES = ["index.wasm","index.pck","index.side.wasm"];
 const FULL_CACHE = CACHED_FILES.concat(CACHEABLE_FILES);
 
 self.addEventListener('install', (event) => {
